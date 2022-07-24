e4e0e67f4616c1357ad3aa98c6c6a483fc66c752
juanmiguel431 <juanmiguel431@gmail.com>
2022-07-24 16:40:16 -0400

    AddingMovies migration added.

---
+++ Vidly/Migrations/202207242037536_AddingMovies.cs
@@ -0,0 +1,44 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingMovies : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        MovieGenreId = c.Int(nullable: false),
+                        ReleasedDate = c.DateTime(nullable: false),
+                        AddedDate = c.DateTime(nullable: false),
+                        Stock = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.MovieGenres", t => t.MovieGenreId, cascadeDelete: true)
+                .Index(t => t.MovieGenreId);
+            
+            CreateTable(
+                "dbo.MovieGenres",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "MovieGenreId", "dbo.MovieGenres");
+            DropIndex("dbo.Movies", new[] { "MovieGenreId" });
+            DropTable("dbo.MovieGenres");
+            DropTable("dbo.Movies");
+        }
+    }
+}
---
+++ Vidly/Models/MovieGenre.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class MovieGenre
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+    }
+}

790d8748b812396d2ddaba867f14eab26795dc74
juanmiguel431 <juanmiguel431@gmail.com>
2022-07-24 16:46:13 -0400

    SeedingMovieGenres migration added.

---
+++ Vidly/Migrations/202207242041277_SeedingMovieGenres.cs
@@ -0,0 +1,21 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedingMovieGenres : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO dbo.MovieGenres (Name) VALUES ('Comedy')");
+            Sql("INSERT INTO dbo.MovieGenres (Name) VALUES ('Action')");
+            Sql("INSERT INTO dbo.MovieGenres (Name) VALUES ('Family')");
+            Sql("INSERT INTO dbo.MovieGenres (Name) VALUES ('Romance')");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE dbo.MovieGenres WHERE Id in (1, 2, 3, 4) ");
+        }
+    }
+}

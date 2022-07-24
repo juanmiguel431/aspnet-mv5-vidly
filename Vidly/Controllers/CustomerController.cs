e6d098c6f3a4124330e9062d0bc09b949afbbbb5
juanmiguel431 <juanmiguel431@gmail.com>
2022-07-23 20:21:27 -0400

    First commit

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private List<Customer> Customers { get; } = new List<Customer>()
+        {
+            new Customer() { Id = 1, Name = "John Smith" },
+            new Customer() { Id = 2, Name = "Mary Williams" }
+        };
+
+        // GET
+        public ViewResult Index()
+        {
+            var model = new CustomerViewModel() { Customers = Customers };
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = Customers.FirstOrDefault(p => p.Id == id);
+            if (customer == null) return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private List<Movie> Movies { get; } = new List<Movie>()
+        {
+            new Movie() { Id = 1, Name = "Shrek" },
+            new Movie() { Id = 2, Name = "Wall-e" }
+        };
+
+        // GET: Movies/Random
+        public ViewResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            // var view = new ViewResult();
+
+            // return View(movie);
+            //return Content("Juan Miguel");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            // return RedirectToAction("Index", "Home", new { Page = 1, OrderBy = "Name" });
+            // return Redirect("/Home");
+            // return Json(movie, JsonRequestBehavior.AllowGet);
+
+            var customers = new List<Customer>()
+            {
+                new Customer() { Name = "Customer 1" },
+                new Customer() { Name = "Customer 2" },
+                new Customer() { Name = "Customer 3" },
+                new Customer() { Name = "Customer 4" },
+                new Customer() { Name = "Customer 5" },
+                new Customer() { Name = "Customer 6" },
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            ViewData["Movie"] = movie;
+            ViewBag.Movie = movie;
+
+            var viewResult = new ViewResult
+            {
+                ViewData =
+                {
+                    Model = viewModel
+                }
+            };
+
+            viewResult.ViewData["Movie"] = movie;
+            // return viewResult;
+            
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content($"id = {movieId}");
+        }
+
+        // public ActionResult Index(int? pageIndex, string sortBy)
+        // {
+        //     if (!pageIndex.HasValue) pageIndex = 1;
+        //
+        //     if (string.IsNullOrWhiteSpace(sortBy)) sortBy = "Name";
+        //
+        //     return Content($"PageIndex={pageIndex}&SortBy={sortBy}");
+        // }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content($"Year:{year} Month:{month}");
+        }        
+        
+        public ActionResult Index()
+        {
+            var model = new MoviesViewModel() { Movies = Movies };
+            return View(model);
+        }
+        
+        public ActionResult Details(int id)
+        {
+            var customer = Movies.FirstOrDefault(p => p.Id == id);
+            if (customer == null) return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}

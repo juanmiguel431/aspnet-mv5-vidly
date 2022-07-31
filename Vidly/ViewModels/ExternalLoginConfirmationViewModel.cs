138c134d85863ae01e921c7059ab561324bacf76
juanmiguel431 <juanmiguel431@gmail.com>
2022-07-31 18:18:19 -0400

    ViewModels separated into single files and moved to ViewModel folder.

---
+++ Vidly/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        
+        [Required, StringLength(255), Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        [Required, StringLength(50)]
+        public string PhoneNumber { get; set; }
+    }
+}

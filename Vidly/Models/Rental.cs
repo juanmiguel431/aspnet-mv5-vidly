f2a3fe0ce3cf44e1ec16f39af3df2fbefe255574
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-01 21:15:27 -0400

    Rental model created.

---
+++ Vidly/Models/Rental.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        
+        public Customer Customer { get; set; }
+        
+        [Required]
+        public long CustomerId { get; set; }
+        
+        public Movie Movie { get; set; }
+        
+        [Required]
+        public int MovieId { get; set; }
+        
+        [Required, Display(Name = "Date rented")]
+        public DateTime DateRented { get; set; }
+        
+        [Display(Name = "Date returned")]
+        public DateTime? DateReturned { get; set; }
+    }
+}

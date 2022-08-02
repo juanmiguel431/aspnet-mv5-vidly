6a7811ec1fa2aa73f1dea22dab2e6f713802c903
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-01 21:44:14 -0400

    RentalController - Add method - Logic added.

--- Vidly/Controllers/Api/RentalController.cs
+++ Vidly/Controllers/Api/RentalController.cs
@@ -1,18 +1,50 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using AutoMapper;
 using Vidly.Dtos;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class RentalController : ApiController
     {
+        private readonly IMapper _mapper;
+        private readonly ApplicationDbContext _context;
+
+        public RentalController(IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = new ApplicationDbContext();
+        }
+
         [HttpPost]
         public IHttpActionResult Add(RentalDto model)
         {
             if (!ModelState.IsValid)
                 return BadRequest();
 
-            var location = $"{Request.RequestUri.AbsoluteUri}/{model.Id}";
-            return Created(location, model);
+            var customer = _context.Customers.Single(p => p.Id == model.CustomerId);
+
+            var movies = _context.Movies.Where(p => model.MovieIds.Contains(p.Id)).ToList();
+
+            var rentals = new List<Rental>();
+            foreach (var movieId in model.MovieIds)
+            {
+                var movie = movies.First(p => p.Id == movieId);
+                
+                rentals.Add(new Rental
+                {
+                    Customer = customer,
+                    Movie = movie
+                });
+            }
+
+            _context.Rentals.AddRange(rentals);
+            _context.SaveChanges();
+            
+            return Ok();
         }
     }
 }
--- Vidly/Dtos/RentalDto.cs
+++ Vidly/Dtos/RentalDto.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Dtos
 {
     public class RentalDto
     {
-        public int Id { get; set; }
+        [Required]
         public int CustomerId { get; set; }
+        
+        [Required]
         public List<int> MovieIds { get; set; }
     }
 }

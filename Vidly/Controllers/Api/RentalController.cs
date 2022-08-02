7830b1187536a5089c1e640d2294776cc1c2a2a2
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-01 21:11:39 -0400

    RentalController added.

---
+++ Vidly/Controllers/Api/RentalController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Add(RentalDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var location = $"{Request.RequestUri.AbsoluteUri}/{model.Id}";
+            return Created(location, model);
+        }
+    }
+}
---
+++ Vidly/Dtos/RentalDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Vidly.Dtos
+{
+    public class RentalDto
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+    }
+}

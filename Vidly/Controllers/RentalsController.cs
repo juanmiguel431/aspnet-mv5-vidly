78b0a8ba2f01912e1f7a58a1c6717663b58f19fb
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-01 22:36:03 -0400

    MVC RentalsController added.

---
+++ Vidly/Controllers/RentalsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using AutoMapper;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class RentalsController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ApplicationDbContext _context;
+
+        public RentalsController(IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = new ApplicationDbContext();
+        }
+        
+        public ViewResult New()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
